1710b9d50fbb7a51660a4d03596cc72b483426e2
Aysel Rajabli <95722237+ayselrajabli@users.noreply.github.com>
2022-09-04 15:41:25 +0400

    Create Number of Ways to Reach a Position After Exactly k Steps.py

---
+++ Number of Ways to Reach a Position After Exactly k Steps.py
@@ -0,0 +1,8 @@
+#2400 task
+
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        if (startPos-endPos-k) % 2:
+            return 0
+        else:
+            return comb(k, (endPos - startPos + k) // 2) % 1000000007
