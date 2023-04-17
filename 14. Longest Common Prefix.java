80d81f9051c98e981a4e0d3987b43d563028d0c0
Aysel Rajabli <95722237+ayselrajabli@users.noreply.github.com>
2023-04-17 12:19:00 +0400

    Create 14. Longest Common Prefix.java

---
+++ 14. Longest Common Prefix.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder ans = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length-1];
+        int  i = 0;
+        while(i <first.length() && i<last.length()){
+            if(first.charAt(i)==last.charAt(i)){
+                i++;
+            }
+            else{
+                break;
+            }
+        }
+        return first.substring(0,i);
+        
+    }
+}
